db721802e34b9e07f8def1caddce5e6c35455890
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-19 21:49:48 -0700

    cleanup

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import ProjectsPage from './pages/ProjectsPage';
 import projects from "../projects.json";
 import Card from "../components/Card/Card"
 
 const ProjectsPage = (props) => {
-    console.log(projects);
-    let project = projects[0]
+    
     return (
-        // <Card/>
+        
         <div>
             {projects.map(project =>
                 <Card
